c446d9c6955c27b4be97be4ec95e2ddba194fc48
cojasu <cjs110@zips.uakron.edu>
2015-01-29 12:57:44 -0500

    Main Character and Movement
    Added the Player Character as well as support for movement.

---
+++ Assets/Scripts/AnimateSprite.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var uvAnimationTileX = 4;
+var uvAnimationTileY = 4;
+var framesPerSecond = 10.0;
+
+function Update () {
+
+	var index : int = Time.time * framesPerSecond;
+	index = index % (4 * 4);
+	var size = Vector2(1.0 / uvAnimationTileX, 1.0 / uvAnimationTileY);
+	var uIndex = index % uvAnimationTileX;
+	var vIndex = index / uvAnimationTileX;
+	var offset = Vector2(uIndex * size.x, 1.0 - size.y - vIndex * size.y);
+	renderer.material.SetTextureOffset ("_MainTex", offset);
+	renderer.material.SetTextureScale("_MainTex", size);
+}
---
+++ Assets/Scripts/PlayerStats.js
@@ -0,0 +1,58 @@
+﻿#pragma strict
+
+var startPoint : Vector3;
+var endPoint : Vector3;
+var speed : float;
+private var increment : float;
+var isMoving : boolean;
+
+function Start () {
+
+startPoint = transform.position;
+endPoint = transform.position;
+speed = 5;
+}
+
+function Update () {
+
+	if(increment <= 1 && isMoving == true){
+		increment += speed/100;
+		Debug.Log("Moving");
+	}
+	else{
+		isMoving = false;
+		Debug.Log("Stopped");
+	}
+	
+	if(isMoving){
+		transform.position = Vector3.Lerp(startPoint,endPoint, increment);
+	}
+	
+	if(Input.GetKey("w") && isMoving == false){
+		increment = 0;
+		isMoving = true;
+		startPoint= transform.position;
+		endPoint= new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+	}
+	
+		if(Input.GetKey("s") && isMoving == false){
+		increment = 0;
+		isMoving = true;
+		startPoint= transform.position;
+		endPoint= new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+	}
+	
+		if(Input.GetKey("a") && isMoving == false){
+		increment = 0;
+		isMoving = true;
+		startPoint= transform.position;
+		endPoint= new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+	}
+	
+		if(Input.GetKey("d") && isMoving == false){
+		increment = 0;
+		isMoving = true;
+		startPoint= transform.position;
+		endPoint= new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+	}
+}
